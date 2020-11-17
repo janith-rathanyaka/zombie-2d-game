620d9ef897f5d49f38eb94387456ac2f64c67038
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2020-11-17 18:49:38 +0530

    build 2d game

---
+++ Assets/Resources/script/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Zombies;
+    public bool isRising = false;
+    public bool isFalling = false;
+    public int activeZombieIndex = 0;
+    Vector2 startPosition;
+    public int riseSpeed = 1;
+    void Start()
+    {
+        pickNewZombies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRising)
+        {
+
+           // Zombies[activeZombieIndex].transform.Translate(Vector2.up*Time.deltaTime*riseSpeed);
+
+            if(Zombies[activeZombieIndex].transform.position.y - startPosition.y >= 1.7f)
+            {
+                isRising = false;
+                isFalling = true;
+            }
+            else
+            {
+                Zombies[activeZombieIndex].transform.Translate(Vector2.up * Time.deltaTime * riseSpeed);
+            }
+
+        }else if (isFalling)
+        {
+
+            if(Zombies[activeZombieIndex].transform.position.y - startPosition.y <= 0f)
+            {
+                isFalling = false;
+                isRising = false;
+            }
+            else
+            {
+                Zombies[activeZombieIndex].transform.Translate(Vector2.down * Time.deltaTime * riseSpeed);
+            }
+
+        }
+        else
+        {
+            Zombies[activeZombieIndex].transform.position = startPosition;
+            pickNewZombies();
+        }
+    }
+
+    private void pickNewZombies()
+    {
+        isRising = true;
+        isFalling = false;
+        activeZombieIndex = UnityEngine.Random.Range(0,Zombies.Length);
+        //GameObject activeZombies = Zombies[activeZombieIndex];
+        //Transform activeTransform = activeZombies.transform;
+        //Vector2 startPosition = activeTransform.position;
+        startPosition = Zombies[activeZombieIndex].transform.position;
+    }
+}
---
+++ Assets/Resources/script/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin,ray.direction);
+
+            if(hit.collider != null)
+            {
+                if(hit.collider.tag == "Enemy")
+                {
+                    Debug.Log("we will hit something");
+                }
+            }
+        }
+    }
+}
